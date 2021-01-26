2598694fe238520c5ad513954c76988cb56e698a
InsDel2113 <52561077+InsDel2113@users.noreply.github.com>
2021-01-26 13:55:34 -0700

    Update helpers.lua

--- helpers.lua
+++ helpers.lua
@@ -1,4 +1,8 @@
+local server_msg_format = "[SERVER]: "
+-- some "wrappers" for some stuff
 function server_broadcast(input)
-    send_message_broadcast("[SERVER]: " .. input)
-  end
-  
+  send_message_broadcast(server_msg_format .. input)
+end
+function server_dm(user_to, input)
+  user_to:sendChatMessage(server_msg_format .. input)
+end
